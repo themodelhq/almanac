4c1cbb03c87958fab8ade92f811a1e2b9f72bef0
themodelhq <themodelhq@gmail.com>
2026-07-19 13:15:41 +0100

    Delete netlify/functions directory

--- netlify/functions/providers.js
+++
@@ -1,9 +0,0 @@
-const { availableProviders } = require('../../lib/providers');
-
-exports.handler = async () => {
-  return {
-    statusCode: 200,
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({ providers: availableProviders() })
-  };
-};
